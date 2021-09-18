3f8912498db878b5ce4b1acd575ccc60c3cb53ae
loloololll <90935071+loloololll@users.noreply.github.com>
2021-09-18 13:32:59 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -158,7 +158,7 @@ chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path=r'C:Users\lizzy\AppData\Local\Temp\Temp1_chromedriver_win32.zip\chromedriver.exe',chrome_options=chrome_options) #Change it
+driver = webdriver.Chrome(executable_path=r'C:\web-driver\chromedriver.exe',chrome_options=chrome_options) #Change it
 
 driver.get("https://vipto.de/")
 
